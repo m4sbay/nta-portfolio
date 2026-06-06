009f47543414ba76184de775a84fed737983387d
m4sbay <maulanabayu026@example.com>
2026-06-06 15:03:06 +0700

    css

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ const Lanyard = dynamic(() => import("../src/component/Lanyard"), {
 
 export default function Home() {
   return (
-    <main className="min-h-svh overflow-hidden bg-neutral-950">
+    <main className="min-h-svh overflow-hidden bg-[#9FA1FF]">
       <section className="relative flex min-h-svh items-center justify-center">
-        <div className="h-svh w-screen">
+        <div className="h-svh w-screen -translate-y-10 sm:translate-y-0">
           <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
         </div>
       </section>
